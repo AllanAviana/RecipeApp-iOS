8095787096bc392edbd7554daccb6f50fcecbf1b
Allan <allan.viana2003@gmail.com>
2024-12-03 23:47:51 -0300

    Initial commit for RecipeApp-iOS

---
+++ RecipeApp-iOS/RecipeApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp_iOSApp.swift
+//  RecipeApp-iOS
+//
+//  Created by Allan Viana on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
